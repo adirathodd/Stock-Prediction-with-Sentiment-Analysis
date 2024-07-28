aa05865e6b9ef39ffc0525cea533885be09222fc
adirathodd <88114611+adirathodd@users.noreply.github.com>
2024-07-28 16:56:13 -0400

    No more window size from commannd line

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ class Main:
     def __init__(self, ticker):
         self.ticker = ticker
     
-    def run(self, window):
+    def run(self):
         stock = Stock(self.ticker)
-        stock.create(window)
+        stock.create(3)
         stock.sentiment_analysis()
         stock.predict()
         stock.print()
 
 main = Main(str(argv[1]))
-main.run(int(argv[2]))
+main.run()
